9d4db0cb029d8d6355b9022d99d99b9b1ce0239d
Eli Bressert <ebressert@gmail.com>
2011-04-06 14:54:18 +0100

    updated to first alpha

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Luj',
+      version='0.1',
+      description='Python accelerator',
+      author='Eli Bressert',
+      author_email='ebressert@gmail.com',
+      url='https://github.com/ebressert/Luj',
+      packages=['luj'],
+     )
